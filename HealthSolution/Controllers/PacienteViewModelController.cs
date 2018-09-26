b0cb730461517888868cf3049a2c6d10097783d1
João Pedro <jpac1207@gmail.com>
2018-09-26 00:07:36 -0300

    begin pacient creation

---
+++ HealthSolution/Controllers/PacienteViewModelController.cs
@@ -0,0 +1,269 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using HealthSolution.Dal;
+using HealthSolution.ViewModels;
+using HealthSolution.Models;
+
+namespace HealthSolution.Controllers
+{
+    public class PacienteViewModelController : Controller
+    {
+        private HealthContext db = new HealthContext();
+
+        // GET: PacienteViewModel
+        public ActionResult Index()
+        {
+            var pacientesViewModel = new List<PacienteViewModel>();
+            var pacientes = db.Pacientes.ToList();
+
+            pacientes.ForEach(x =>
+            {
+                PacienteViewModel pacienteViewModel = GetPacienteViewModel(x);
+                pacientesViewModel.Add(pacienteViewModel);
+            });
+
+            return View(pacientesViewModel);
+        }
+
+        private PacienteViewModel GetPacienteViewModel(Paciente x)
+        {
+            var myPhone = db.Telefones.Where(y => y.PacienteId == x.Id).FirstOrDefault();
+            var myAddress = db.Enderecos.Where(y => y.PacienteId == x.Id).FirstOrDefault();
+
+            var pacienteViewModel = new PacienteViewModel();
+            pacienteViewModel.Id = x.Id;
+            pacienteViewModel.Nome = x.Nome;
+            pacienteViewModel.Cpf = x.Cpf;
+            pacienteViewModel.DataNascimento = x.DataNascimento;
+            pacienteViewModel.DataCadastro = x.DataCadastro;
+            pacienteViewModel.ComoConheceu = x.ComoConheceu;
+
+            if (myPhone != null)
+            {
+                pacienteViewModel.NumeroTelefone = myPhone.Numero;
+            }
+
+            if (myAddress != null)
+            {
+                pacienteViewModel.Cidade = myAddress.Cidade;
+                pacienteViewModel.Bairro = myAddress.Bairro;
+                pacienteViewModel.Rua = myAddress.Rua;
+                pacienteViewModel.NumeroResidencia = myAddress.Numero;
+            }
+
+            return pacienteViewModel;
+        }
+
+        // GET: PacienteViewModel/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PacienteViewModel pacienteViewModel = GetPacienteViewModel(db.Pacientes.Find(id));
+            if (pacienteViewModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pacienteViewModel);
+        }
+
+        // GET: PacienteViewModel/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: PacienteViewModel/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Nome,Cpf,DataNascimento,DataCadastro,ComoConheceu,Cidade,Bairro,Rua,NumeroResidencia,NumeroTelefone")] PacienteViewModel pacienteViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                using (var transaction = db.Database.BeginTransaction())
+                {
+                    try
+                    {
+                        var paciente = new Paciente();
+                        paciente.Nome = pacienteViewModel.Nome;
+                        paciente.Cpf = pacienteViewModel.Cpf;
+                        paciente.DataNascimento = pacienteViewModel.DataNascimento;
+                        paciente.DataCadastro = pacienteViewModel.DataCadastro;
+                        paciente.ComoConheceu = pacienteViewModel.ComoConheceu;
+                        db.Pacientes.Add(paciente);
+                        db.SaveChanges();
+
+                        var endereco = new Endereco();
+                        endereco.Cidade = pacienteViewModel.Cidade;
+                        endereco.Bairro = pacienteViewModel.Bairro;
+                        endereco.Rua = pacienteViewModel.Rua;
+                        endereco.Numero = pacienteViewModel.NumeroResidencia;
+                        endereco.PacienteId = paciente.Id;
+                        db.Enderecos.Add(endereco);
+
+                        var telefone = new Telefone();
+                        telefone.Numero = pacienteViewModel.NumeroTelefone;
+                        telefone.PacienteId = paciente.Id;
+                        db.Telefones.Add(telefone);
+
+                        db.SaveChanges();
+                        transaction.Commit();
+                    }
+                    catch (Exception e)
+                    {
+                        transaction.Rollback();
+                    }
+                }
+
+                return RedirectToAction("Index");
+            }
+
+            return View(pacienteViewModel);
+        }
+
+        // GET: PacienteViewModel/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PacienteViewModel pacienteViewModel = GetPacienteViewModel(db.Pacientes.Find(id));
+            if (pacienteViewModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pacienteViewModel);
+        }
+
+        // POST: PacienteViewModel/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Nome,Cpf,DataNascimento,DataCadastro,ComoConheceu,Cidade,Bairro,Rua,NumeroResidencia,NumeroTelefone")] PacienteViewModel pacienteViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                using (var transaction = db.Database.BeginTransaction())
+                {
+                    try
+                    {
+                        var paciente = db.Pacientes.Where(x => x.Id == pacienteViewModel.Id).FirstOrDefault();
+
+                        if (paciente != null)
+                        {
+                            paciente.Nome = pacienteViewModel.Nome;
+                            paciente.Cpf = pacienteViewModel.Cpf;
+                            paciente.DataNascimento = pacienteViewModel.DataNascimento;
+                            paciente.DataCadastro = pacienteViewModel.DataCadastro;
+                            paciente.ComoConheceu = pacienteViewModel.ComoConheceu;
+
+                            var endereco = db.Enderecos.Where(x => x.PacienteId == paciente.Id).FirstOrDefault();
+
+                            if (endereco != null)
+                            {
+                                endereco.Cidade = pacienteViewModel.Cidade;
+                                endereco.Bairro = pacienteViewModel.Bairro;
+                                endereco.Rua = pacienteViewModel.Rua;
+                                endereco.Numero = pacienteViewModel.NumeroResidencia;
+                                endereco.PacienteId = paciente.Id;
+                            }
+
+                            var telefone = db.Telefones.Where(x => x.PacienteId == paciente.Id).FirstOrDefault();
+
+                            if (telefone != null)
+                            {
+                                telefone.Numero = pacienteViewModel.NumeroTelefone;
+                                telefone.PacienteId = paciente.Id;
+                            }
+
+                            db.SaveChanges();
+                            transaction.Commit();
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        transaction.Rollback();
+                    }
+                }
+                return RedirectToAction("Index");
+            }
+            return View(pacienteViewModel);
+        }
+
+        // GET: PacienteViewModel/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PacienteViewModel pacienteViewModel = GetPacienteViewModel(db.Pacientes.Find(id));
+            if (pacienteViewModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pacienteViewModel);
+        }
+
+        // POST: PacienteViewModel/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            using (var transaction = db.Database.BeginTransaction())
+            {
+                try
+                {
+                    var paciente = db.Pacientes.Where(x => x.Id == id).FirstOrDefault();
+
+                    if (paciente != null)
+                    {                       
+                        var endereco = db.Enderecos.Where(x => x.PacienteId == paciente.Id).FirstOrDefault();
+
+                        if (endereco != null)
+                        {
+                            db.Enderecos.Remove(endereco);
+                        }
+
+                        var telefone = db.Telefones.Where(x => x.PacienteId == paciente.Id).FirstOrDefault();
+
+                        if (telefone != null)
+                        {
+                            db.Telefones.Remove(telefone);
+                        }
+
+                        db.Pacientes.Remove(paciente);
+                        db.SaveChanges();
+                        transaction.Commit();
+                    }
+                }
+                catch (Exception e)
+                {
+                    transaction.Rollback();
+                }
+            }
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ HealthSolution/ViewModels/PacienteViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HealthSolution.ViewModels
+{
+    public class PacienteViewModel
+    {
+        //just to use the framework with scafolding
+        public int Id { get; set;}
+
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public DateTime DataCadastro { get; set; }
+        public string ComoConheceu { get; set; }
+
+        public string Cidade { get; set; }
+        public string Bairro { get; set; }
+        public string Rua { get; set; }
+        public string NumeroResidencia { get; set; }
+
+        public string NumeroTelefone { get; set; }
+               
+    }
+}

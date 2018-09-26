01ef1d0a968911e3bc7eb2fcbf3e31d9ee5ad290
João Pedro <jpac1207@gmail.com>
2018-09-26 19:39:29 -0300

    Creating ViewModel

---
+++ HealthSolution/ViewModels/EspecialistaViewModel.cs
@@ -0,0 +1,16 @@
+﻿using HealthSolution.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HealthSolution.ViewModels
+{
+    public class EspecialistaViewModel
+    {
+        public int Id { get; set; }
+        public int Nome { get; set; }
+        public string Crm { get; set; }
+        public List<Especialidade> Especialidades { get; set; }
+    }
+}
